4631ca646d1528fba9af14fdefb99326bd87d0a1
emmarcaber <caberemmar@gmail.com>
2025-04-12 21:27:55 +0800

    feat: add externals assets (e.g. styling, scripts, logo)

--- public/js/script.js
+++ public/js/script.js
@@ -4097,3 +4097,21 @@
   );
 });
 //# sourceMappingURL=bootstrap.bundle.min.js.map
+
+window.addEventListener('DOMContentLoaded', event => {
+
+  // Toggle the side navigation
+  const sidebarToggle = document.body.querySelector('#sidebarToggle');
+  if (sidebarToggle) {
+      // Uncomment Below to persist sidebar toggle between refreshes
+      // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+      //     document.body.classList.toggle('sb-sidenav-toggled');
+      // }
+      sidebarToggle.addEventListener('click', event => {
+          event.preventDefault();
+          document.body.classList.toggle('sb-sidenav-toggled');
+          localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+      });
+  }
+
+});
