4c5efed7129c0bea2a7e96dfc4feb0865973e5ad
emmarcaber <caberemmar@gmail.com>
2025-04-19 23:40:16 +0800

    feat: add profile link to user dropdown in header

--- app/Views/templates/customer/header.php
+++ app/Views/templates/customer/header.php
@@ -26,6 +26,7 @@
                         <div class="dropdown text-white">
                             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-fw"></i></a>
                             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Profile</a></li>
                                 <li><a class="dropdown-item" href="<?= route_to('auth.logout') ?>">Logout</a></li>
                             </ul>
                         </div>
